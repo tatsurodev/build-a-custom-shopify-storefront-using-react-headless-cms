7cddd39bb19737026d3edda3cc753012a9364033
Tatsuro <contacts@tatsuro.dev>
2021-12-26 03:56:10 +0900

    shopify関連のdataをcontextにまとめる 5

--- shopify-react/src/context/shopContext.js
+++ shopify-react/src/context/shopContext.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react'
 import Client from 'shopify-buy'
 
+const ShopContext = React.createContext()
+
 // https://shopify.github.io/js-buy-sdk/
 const client = Client.buildClient({
-  domain: 'your-shop-name.myshopify.com',
-  storefrontAccessToken: 'your-storefront-access-token',
+  domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API,
 })
 
 export class ShopProvider extends Component {
+  state = {
+    product: {},
+    products: [],
+    checkout: {},
+    isCartOpen: false,
+    isMenuOpen: false,
+  }
+
+  createCheckout = async () => {}
+
+  fetchCheckout = async () => {}
+
+  addItemToCheckout = async () => {}
+
+  removeLineItem = async (lineItemIdsToRemove) => {}
+
+  fetchAllProducts = async () => {}
+
+  fetchProductWithHandle = async (handle) => {}
+
+  closeCart = () => {}
+
+  openCart = () => {}
+
+  closeMenu = () => {}
+
+  openMenu = () => {}
+
   render() {
-    return <div></div>
+    return <ShopContext.Provider>{this.props.children}</ShopContext.Provider>
   }
 }
+
+const ShopConsumer = ShopContext.Consumer
+
+export { ShopConsumer, ShopContext }
 
 export default ShopProvider
