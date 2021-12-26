603e47f5c21e5d85a7162dfb9e32ec0820cf8a2a
Tatsuro <contacts@tatsuro.dev>
2021-12-26 09:26:36 +0900

    chakra uiの使用 8

--- shopify-react/src/pages/Home.js
+++ shopify-react/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { Box, Grid, Text, Image } from '@chakra-ui/react'
 import { ShopContext } from '../context/shopContext'
 
 const Home = () => {
@@ -12,13 +13,19 @@ const Home = () => {
   if (!products) return <div>Loadings...</div>
 
   return (
-    <div>
-      {products.map((product) => (
-        <Link to={`/products/${product.handle}`} key={product.title}>
-          {product.title}
-        </Link>
-      ))}
-    </div>
+    <Box>
+      <Grid templateColumns='repeat(3,1fr)'>
+        {products.map((product) => (
+          <Link to={`/products/${product.handle}`} key={product.id}>
+            <Box _hover={{ opacity: '80%' }} textAlign='center'>
+              <Image src={product.images[0].src} />
+              <Text>{product.title}</Text>
+              <Text>${product.variants[0].price}</Text>
+            </Box>
+          </Link>
+        ))}
+      </Grid>
+    </Box>
   )
 }
 
