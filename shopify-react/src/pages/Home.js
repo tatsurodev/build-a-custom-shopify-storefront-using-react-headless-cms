f9baa1f1668b1bfd2c7132161c312a32070393f0
Tatsuro <contacts@tatsuro.dev>
2021-12-27 02:40:39 +0900

    ImageWithText作成 15

--- shopify-react/src/pages/Home.js
+++ shopify-react/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Box, Grid, Text, Image } from '@chakra-ui/react'
 import Hero from '../components/Hero'
+import ImageWithText from '../components/ImageWithText'
 import { ShopContext } from '../context/shopContext'
 
 const Home = () => {
@@ -27,6 +28,19 @@ const Home = () => {
           </Link>
         ))}
       </Grid>
+      <ImageWithText
+        button
+        image='https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758'
+        heading='Heading'
+        text="I'm baby kale chips twee skateboard tattooed, DIY iPhone ugh mixtape tumeric unicorn narwhal. Iceland shoreditch authentic, sartorial vegan twee flannel banh mi bushwick retro farm-to-table single-origin coffee. "
+      />
+      <ImageWithText
+        reverse
+        button
+        image='https://cdn.shopify.com/s/files/1/0472/5705/9496/files/bath-bomb-and-candle.jpg?v=1610066758'
+        heading='Second Heading'
+        text="I'm baby kale chips twee skateboard tattooed, DIY iPhone ugh mixtape tumeric unicorn narwhal. Iceland shoreditch authentic, sartorial vegan twee flannel banh mi bushwick retro farm-to-table single-origin coffee. "
+      />
     </Box>
   )
 }
