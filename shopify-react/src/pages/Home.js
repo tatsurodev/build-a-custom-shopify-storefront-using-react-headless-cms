a266e25e87d45258392c729f2ee9c07eff6c6090
Tatsuro <contacts@tatsuro.dev>
2021-12-27 02:54:28 +0900

    RichText作成 16

--- shopify-react/src/pages/Home.js
+++ shopify-react/src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { Box, Grid, Text, Image } from '@chakra-ui/react'
 import Hero from '../components/Hero'
 import ImageWithText from '../components/ImageWithText'
+import RichText from '../components/RichText'
 import { ShopContext } from '../context/shopContext'
 
 const Home = () => {
@@ -17,17 +18,30 @@ const Home = () => {
   return (
     <Box>
       <Hero />
+      <RichText
+        heading="The relaxation you've been waiting for."
+        text='Our Bath bombs guarantee a fun, relaxing, and colorful night.'
+      />
       <Grid templateColumns='repeat(3,1fr)'>
         {products.map((product) => (
           <Link to={`/products/${product.handle}`} key={product.id}>
-            <Box _hover={{ opacity: '80%' }} textAlign='center'>
+            <Box
+              _hover={{ opacity: '80%' }}
+              textAlign='center'
+              position='relative'
+            >
               <Image src={product.images[0].src} />
-              <Text>{product.title}</Text>
-              <Text>${product.variants[0].price}</Text>
+              <Text position='absolute' bottom='15%' w='100%' fontWeight='bold'>
+                {product.title}
+              </Text>
+              <Text position='absolute' bottom='5%' w='100%' color='gray.500'>
+                ${product.variants[0].price}
+              </Text>
             </Box>
           </Link>
         ))}
       </Grid>
+      <RichText heading='Treat yourself!' />
       <ImageWithText
         button
         image='https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758'
